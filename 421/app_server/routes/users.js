78bcbcaa433fde4391a762f135c410f698532db1
pmccullough2204 <mcculloughpatr@gmail.com>
2024-02-19 13:07:52 -0500

    More refactoring to include models and controllers

--- 421/app_server/routes/users.js
+++ 421/app_server/routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var ctrlUsers = require('../controllers/usersCtrl.js');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+/* GET home page. */
+router.get('/', ctrlUsers.index);
 
 module.exports = router;
+
