a6725a5ba2fca26ab89a02c98437839c8933bb7f
pmccullough2204 <mcculloughpatr@gmail.com>
2024-02-19 12:18:15 -0500

    Refactored to include app_server

--- 421/app.js
+++ 421/app.js
@@ -4,14 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var blogAddRouter = require('./routes/blogAdd');
-var blogListRouter = require('./routes/blogList');
+var indexRouter = require('./app_server/routes/index');
+var usersRouter = require('./app_server/routes/users');
+var blogAddRouter = require('./app_server/routes/blogAdd');
+var blogListRouter = require('./app_server/routes/blogList');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname,'app_server', 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
